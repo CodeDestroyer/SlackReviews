77ccb20df8b9ef771bc93bdc72bc51d2a95456a6
Patrick Cunningham <pcunningham@pet360.com>
2015-02-16 21:16:21 -0500

    removed stuff from git ignore

--- .env.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-
-    'DB_USER' => 'homestead',
-    'DB_PASSWORD' => 'secret',
-    'SLACK_TOKEN' => 'HUBOT_SLACK_TOKEN=xoxb-3710053494-P6PiI4KYE4GBe7yzmuV4Exhk ./bin/hubot --adapter slack',
-    'CODE_REVIEW_CHANNEL' => '#codetest'
-);
--- .env.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-
-    'DB_USER' => 'root',
-    'DB_PASSWORD' => 'ch@ng3m3',
-    'SLACK_TOKEN' => 'HUBOT_SLACK_TOKEN=xoxb-3710053494-P6PiI4KYE4GBe7yzmuV4Exhk ./bin/hubot --adapter slack',
-);
