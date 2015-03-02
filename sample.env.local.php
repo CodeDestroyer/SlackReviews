6d2447ac3d54b27c19b58f26434d65e43362a257
Patrick Cunningham <pcunningham@pet360.com>
2015-03-01 21:38:06 -0500

    added sample config file

---
+++ sample.env.local.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+    'DB_USER' => 'DATABASEUSERNAME',
+    'DB_PASSWORD' => 'DATABASEPASSWORD',
+    'deployChannel' => 'YOURPRIVATETESTINGCHANNEL',
+    'reviewChannel' => 'YOURPRIVATETESTINGCHANNEL',
+    'slackEndPoint' => 'TESTCODEDADENDPOINT'
+);
